9598551eb41a20cee2ddf6deae2d82283021ca13
Devpacito <dev.awesome9@gmail.com>
2020-02-18 16:57:57 -0600

    Adding aiming and shooting in Auton

--- src/main/java/frc/team3130/robot/commands/Auton/TurnAndShoot.java
+++ src/main/java/frc/team3130/robot/commands/Auton/TurnAndShoot.java
@@ -6,11 +6,11 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.team3130.robot.commands.Turret.ToggleTurretAim;
 import frc.team3130.robot.subsystems.Chassis;
 import frc.team3130.robot.subsystems.Turret;
+import frc.team3130.robot.subsystems.Flywheel;
 
 import java.util.Set;
 
-import static frc.team3130.robot.subsystems.Turret.isTurretAiming;
-import static frc.team3130.robot.subsystems.Turret.setAimState;
+import static frc.team3130.robot.subsystems.Turret.*;
 
 public class TurnAndShoot implements Command {
     private final Set<Subsystem> subsystems;
@@ -19,15 +19,29 @@ public class TurnAndShoot implements Command {
     public TurnAndShoot() {
         this.subsystems = Set.of(Turret.getInstance());
 
+
     }
+
+    double time;
+
     public void initialize(){
+        time = Timer.getFPGATimestamp();
+
         Turret.toggleAimState();
 
     }
 
     public void execute(){
-        
+        double currentTime;
+        currentTime = Timer.getFPGATimestamp();
+        double differenceInTime = currentTime - time;
+
+        if(Turret.isOnTarget() == true && differenceInTime >= 0.3 && differenceInTime <= 3.0) {
+            Flywheel.setOpenLoop(0.7);
+        }
+
     }
+
 
 
     @Override
